bf352a6bbc83913309351c1c44dfb2beb9b84862
Adam Becker <ad@mbecker.cc>
2015-06-23 23:25:04 -0400

    urgh

--- throttle-promises/index.js
+++ throttle-promises/index.js
@@ -1,26 +1,26 @@
 module.exports = function(max, funcs) {
   return new Promise(function(resolve){
     var current = 0;
+    var counter = 0;
     var results = [];
-    var needed = funcs.length;
     next();
 
     function next() {
-      if (!funcs.length) return;
-      if (current < 5) {
-        current++;
-        funcs.shift()().then(function(result){
-          results.push(result);
-          current--;
-          if (results.length === needed) {
-            console.log('resolve');
-            resolve(results);
-          } else {
-            console.log('next');
+      if (funcs.length) {
+        if (current < 5) {
+          current++;
+          var myCounter = counter++;
+          funcs.shift()().then(function(result){
+            current--;
+            results[myCounter] = result;
             next();
-          }
-        });
-        next();
+          });
+          next();
+        }
+      } else {
+        if (current === 0) {
+          resolve(results);
+        }
       }
     }
   });
